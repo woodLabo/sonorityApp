efcc938f2fbae50ff38ead8ece5494affddcb43b
m_seiji <web.works.ms@gmail.com>
2020-10-22 01:02:42 +0900

    headerの関係ファイルを設置

---
+++ js/Components/Header/Title.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Title extends React.Component {
+	render() {
+		return
+	}
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Layout from "./Components/Layout";
 
 const app = document.querySelector("#app");
 ReactDOM.render(<Layout/>, app);
